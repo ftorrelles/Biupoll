4fde700213867fae40e996ec3b409aac8120714e
ftorrelles <torrellesf93@gmai.com>
2025-04-25 16:34:22 -0500

    change in contentAbout.css url

--- src/components/ContentAbout.jsx
+++ src/components/ContentAbout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
-import "../styles/ContentAbout.css";
+import "../styles/contentAbout.css";
 
 const ContentAbout = () => {
   return (
