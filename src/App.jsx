acd210468ad731c982aa6bd296bad195ddb2531a
ftorrelles <torrellesf93@gmai.com>
2025-04-25 16:25:16 -0500

    test footer export

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,8 @@
-// import "./App.css";
-// import { HashRouter, Routes, Route } from "react-router-dom";
-// import NavBar from "./compenents/NavBar";
-// import Home from "./pages/Home";
-// import About from "./pages/About";
-// import Contact from "./pages/Contact";
-// import Footer from "./compenents/Footer";
-
-// function App() {
-//   return (
-//     <HashRouter>
-//       <div className="App">
-//         <NavBar />
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/about" element={<About />} />
-//           <Route path="/contact" element={<Contact />} />
-//         </Routes>
-//         <Footer />
-//       </div>
-//     </HashRouter>
-//   );
-// }
-
-// export default App;
-
 import "./App.css";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import NavBar from "./components/NavBar";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
 import Services from "./pages/Services";
 import About from "./pages/About";
 import BusinessCard from "./pages/BusinessCard";
